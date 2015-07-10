5f6ee428acf66f5b03edae3708376050a353fce6
Andy Liang <andy@layer3tv.com>
2015-07-10 14:12:00 -0400

    Create dump_history.py

---
+++ gerrit/dump_history.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+#dumps all history from tables gerrit's shitty sql impl into a bunch of json files
+
+import os
+import json
+
+#for now make the following assumptions
+PATH_TO_GERRIT_DIR = '..'
+PATH_TO_GERRIT_WAR = './gerrit.war'
+
+BASE_COMMAND_JSON = 'java -jar ' + PATH_TO_GERRIT_WAR + ' gsql -c "%(sql_command)s" --format JSON -d ' + PATH_TO_GERRIT_DIR
+LIST_TABLES = BASE_COMMAND_JSON % {'sql_command': 'SHOW TABLES;'}
+
+to_import = filter(
+  lambda x: x['type'] == 'row',
+  map(json.loads, os.popen(LIST_TABLES))
+)
+
+for table in to_import:
+  table_name = table['columns']['table_name']
+
+  args = {'table_name': table_name }
+
+  schema_list = os.popen(BASE_COMMAND_JSON % {'sql_command': 'SHOW COLUMNS FROM %(table_name)s;' % args})
+
+  schema = [field['columns']['field'] for field in map(json.loads, schema_list) if field['type'] == 'row']
+
+  file = open(table_name, 'w')
+
+  file.write(','.join(schema))
+  file.write('\n')
+
+  for line in os.popen(BASE_COMMAND_JSON % {'sql_command': 'SELECT * FROM %(table_name)s' % args}):
+    file.write(line)
